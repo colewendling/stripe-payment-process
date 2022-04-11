cb6010382065ec9c8e465821109056351b74760a
colewendling <colewendling@utexas.edu>
2022-04-11 01:01:01 -0500

    improvements

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { resolve } = require("path");
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 //middleware
 app.use(express.static(process.env.STATIC_DIR));
@@ -93,8 +92,7 @@ app.post("/payment-sheet", async (req, res) => {
     setupIntent: setupIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customerObject: customer,
-    publishableKey:
-      "pk_test_51KiQMsI3oyKP8eORrA6f2aREXVZJo16HxStTPqzcZ4TjVgZCRE6VbmMJyQNHv14XKaTRa6orZkRUB4Oxbju76ofP00qt8z5S9T",
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   });
 });
 
@@ -150,57 +148,6 @@ app.post("/echo", (req, res) => {
 // })
 
 //updates
-app.post(
-  "/webhook",
-  bodyParser.raw({ type: "application/json" }),
-  (req, res) => {
-    const sig = req.headers["stripe-signature"];
 
-    let event;
-
-    try {
-      event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
-    } catch (err) {
-      // On error, log and return the error message
-      console.log(`❌ Error message: ${err.message}`);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    if (event.type === "payment_intent.created") {
-      const paymentIntent = event.data.object;
-      console.log(
-        `✅ [${event.id}] PaymentIntent:(${paymentIntent.id}) Status:${paymentIntent.status}`
-      );
-    }
-        if (event.type === "customer.created") {
-          const customer = event.data.object;
-          console.log(
-            `✅ [${event.id}] Customer:(${customer.id}) Created with default_source: (${customer.default_source})`
-          );
-        }
-
-         if (event.type === "paymentMethod.complete") {
-           const paymentMethod = event.data.object;
-           console.log(
-             `✅ [${paymentMethod.id}] paymentMethod_id:(${paymentMethod.id}) Created.`
-           );
-         }
-    if (event.type === "payment_intent.requires_action") {
-      const paymentIntent = event.data.object;
-      console.log(
-        `✅ [${event.id}] PaymentIntent:(${paymentIntent.id}) Status:${paymentIntent.status}`
-      );
-    }
-    if (event.type === "payment_intent.succeeded") {
-      const paymentIntent = event.data.object;
-      console.log(
-        `✅ [${event.id}] PaymentIntent:(${paymentIntent.id}) Status:${paymentIntent.status} ${paymentIntent}`
-      );
-    }
-
-    // Return a response to acknowledge receipt of the event
-    res.json({ received: true });
-  }
-);
 
 app.listen(4242, () => console.log("Running on http://localhost:4242"));
